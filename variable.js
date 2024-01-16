1ab7b6f03c5f86e6ba695a5f9172e09e4caf3162
vishnu <vishnuvijayan123abc@gmail.com>
2024-01-16 08:38:42 +0530

    java

---
+++ variable.js
@@ -0,0 +1,4 @@
+const companyname="Luminar"
+console.log("company name is",companyname);
+companyname="luminar technolab"
+console.log(companyname)
---
+++ work.js
@@ -0,0 +1,5 @@
+var customername="vishnu"
+var amt=320
+var invoice=6197
+
+console.log(`dear ${customername} ,you have made a purchase of RS.${amt} inv.no ${invoice} `);
